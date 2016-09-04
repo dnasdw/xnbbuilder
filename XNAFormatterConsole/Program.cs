6bc129fabda3cca7b75b320d7f56972e8ed9035c
Sun Daowen <sundaowen@gmail.com>
2016-09-04 15:42:20 +0800

    add console version

---
+++ XNAFormatterConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using XNAConverter;
+
+namespace XNAFormatterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return;
+            }
+            XNBBuilder xnbBuilder = new XNBBuilder("Windows", "Reach", false);
+            bool buildStatus = false;
+            string[] fileNames = new string[] { args[0] };
+            xnbBuilder.PackageContent(fileNames, args[1], false, Path.GetDirectoryName(fileNames[0]), out buildStatus);
+            try
+            {
+                File.Delete("ContentPipeline.xml");
+            }
+            catch
+            {
+            }
+        }
+    }
+}
